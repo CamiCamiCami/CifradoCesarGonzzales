81be12dd5d4babbfab0af7cd471d1af1e3580a72
Camilo Sodo <sodocamilo@gmail.com>
2024-04-13 19:00:10 -0300

    Agregue la funcion que Decifra

--- Cifrado.cs
+++ Cifrado.cs
@@ -3,30 +3,52 @@ using System.Text;
 
 
 class Program {
-  static string alfabeto = "abcdefghijklmñnopqrstuvwxyz";
-  public static void Main(){
-      Console.WriteLine("Ingrese la frase a cifrar");
+    static string alfabeto = "abcdefghijklmñnopqrstuvwxyz";
+    public static void Main() {
+        Console.WriteLine("Ingrese la frase a cifrar");
 
-      string msj = Console.ReadLine();
-      while (string.IsNullOrEmpty(msj))
-      {
-          Console.WriteLine("Ingrese un mensaje no vacio");
-          msj = Console.ReadLine();
-      }
-      Program program = new Program();  
-      Console.WriteLine(Program.Cifrar(msj));
-  }
+        string msj = Console.ReadLine();
+        while (string.IsNullOrEmpty(msj))
+        {
+            Console.WriteLine("Ingrese un mensaje no vacio");
+            msj = Console.ReadLine();
+        }
+        Program program = new Program();
 
-  public static string Cifrar(string mensaje){
+        string msj_cifrado = Program.Cifrar(msj);
+        Console.WriteLine(msj_cifrado);
+        string msj_decifrado = Program.Decifrar(msj_cifrado);
+        Console.WriteLine(msj_decifrado);
+    }
+
+    public static string Cifrar(string mensaje) {
         int shift = 3;
         StringBuilder constructor = new StringBuilder();
-        foreach(char c in mensaje){
+        foreach (char c in mensaje) {
             int i = Program.alfabeto.IndexOf(c);
             i += shift;
+            if (i < 0) { i += Program.alfabeto.Length; }
             i %= Program.alfabeto.Length;
             char char_cifrado = Program.alfabeto[i];
             constructor.Append(char_cifrado);
         }
         return constructor.ToString();
     }
+
+    public static string Decifrar(string mensaje)
+    {
+        int shift = -3;
+        StringBuilder constructor = new StringBuilder();
+        foreach (char c in mensaje)
+        {
+            int i = Program.alfabeto.IndexOf(c);
+            i += shift;
+            if (i < 0) { i += Program.alfabeto.Length; }
+            i %= Program.alfabeto.Length;
+            char char_cifrado = Program.alfabeto[i];
+            constructor.Append(char_cifrado);
+        }
+        return constructor.ToString();
+    }
+
 }
